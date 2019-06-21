bf55a5078492758246c9490459070325096389eb
Kira Corbin <madkatkeys@gmail.com>
2019-06-20 21:00:56 -0700

    add bars and buttons, fixed for loop

--- assets/javascript/gifhy.js
+++ assets/javascript/gifhy.js
@@ -1,8 +1,10 @@
+// varibles
+const animals = ["jellyfish", "sharks", "pandas", "panthers", "owls", "unicorns"]
 // creates an animal button
 $(".animalBtn").on("click", function () {
     console.log(this)
     const search = $(this).val().trim();
-    const queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=kVAeSh4zD3QQSz4vVdswwl5QKrFJ8aQt&limit=10";
+    const queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=kVAeSh4zD3QQSz4vVdswwl5QKrFJ8aQt&limit=4";
     // const giphyAPIKey = '&api_key=kVAeSh4zD3QQSz4vVdswwl5QKrFJ8aQt'
 
     $.ajax({
@@ -12,228 +14,33 @@ $(".animalBtn").on("click", function () {
         .then(function (response) {
             console.log(response)
 
-            // . are use to select class with jquery while # is used to select id. ** importan **
-            // testing individual buttons
-            // $("#animalBtn2").on("click", function () {
-            //     console.log('worky')
-            // })
-            // $("#animalBtn3").on("click", function () {
-            //     console.log('worky')
-            // })
-            // $("#animalBtn4").on("click", function () {
-            //     console.log('worky')
-            // })
-            // $("#animalBtn5").on("click", function () {
-            //     console.log('worky')
-            // })
-            // $("#animalBtn6").on("click", function () {
-            //     console.log('worky')
-            // })
+            // loop through images
+            // grab url from fixed height they will be nested
+            for (let i = 0; i < response.data.length; i++) {
+                console.log(response.data[i])
+                const animalImageUrl = response.data[i].images.fixed_height.url;
+                const animalImage = $("<img>");
 
-            // api key 9gBBT9au0Ri9DECTaxgag0fvIw3X6M9w
+                animalImage.attr("src", animalImageUrl);
+                animalImage.attr("alt", "animal images");
 
-
-            const animalImageUrl = response.data.image_original;
-            const animalImage = $("<img>");
-
-            animalImage.attr("src", animalImageUrl);
-            animalImage.attr("alt", "animal images");
-
-            $("#images").prepend(animalImage);
+                $("#images-might-appear-here").prepend(animalImage);
+            }
 
         })
 
-
-    // recovered from github
-    // $('#search').button({
-
-    // }) 
-    // // api key 9gBBT9au0Ri9DECTaxgag0fvIw3X6M9w
-    // let giphyAPIKey = '&api_key=9gBBT9au0Ri9DECTaxgag0fvIw3X6M9w'
-
-    // // creates an animal button
-    // $("animalBtn").on("click", function () {
-    //     var animalChoosen = $(this).attr("value")
-    //     let giphyURL = "https://api.giphy.com/v1/gifs/search?api_key=9gBBT9au0Ri9DECTaxgag0fvIw3X6M9w&q=animals&limit=10&offset=0&rating=PG&lang=en" + animalChoosen +
-    //         giphyAPIKey + "&limit=8";
-
-    //     // console.log(giphyURL)
-
-    //     $.ajax({
-    //         url: giphyURL,
-    //         method: "GET"
-    //     }).then(function (response) {
-    //         // console.log(response.data)
-
-    //         for (var i = 0; i < response.data.length; i++) {
-    //             console.log("hello world")
-
-    //             $("gifs-might-appear-here").attr("src", response.data.url)
-    //             // console.log(response.data)
-    //         }
-
-    //     })
-    // })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-})   
+})
+
+ $("#submit").on("click", function (event) {
+        event.preventDefault();
+        // grab value from anchor
+        newAnimal = $("#bar").val()
+        console.log(newAnimal)
+        newAnimal.text
+        // create a new btn
+        // add button text , set = to newanimal varible.
+        // append btn to page
+
+        // pause buttons
+
+    })
